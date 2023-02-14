4f0f806350d12ee12235195cd9d754f0d9bdfd73
ludo1243 <matveur@yandex.com>
2023-02-14 16:00:35 +0300

    Start

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 2147483647;
+        byte b = 127;
+        short c = 32767;
+        long d = 322337203;
+        float e = 1.7F;
+        double f = 5.3;
+
+        System.out.println("Значение переменной a с типом int равно " + a);
+        System.out.println("Значение переменной b с типом byte равно " + b);
+        System.out.println("Значение переменной c с типом short равно " + c);
+        System.out.println("Значение переменной d с типом long равно " + d);
+        System.out.println("Значение переменной e с типом float равно " + e);
+        System.out.println("Значение переменной f с типом double равно " + f);
+    }
+}
